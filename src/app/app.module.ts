c6691c09733365b8bd70c6c0cf8c1c00a643b917
Yusuf Wijaya <yusufwijaya3@gmail.com>
2025-05-11 23:10:20 +0700

    [FIRST INIT]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { HomeModule } from './home/home.module';
+import { ReservationModule } from './reservation/reservation.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule,
+    ReservationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
